19de19633cd28c1268bc1fdf48bb8a2abf8227d7
Dannii Willis <curiousdannii@gmail.com>
2024-03-17 13:52:51 +1000

    Emglken: Use AsyncDialog

--- remglk_capi/src/systems/library_emglken.js
+++ remglk_capi/src/systems/library_emglken.js
@@ -14,81 +14,57 @@ const EMGLKEN_JS = {
     $EMGLKEN_JS_CONSTANTS__deps: ['$writeBuffer', '$writeBufferJSON'],
     $EMGLKEN_JS_CONSTANTS__postset: `
         const encoder = new TextEncoder()
-
-        const FILETYPES = ['data', 'save', 'transcript', 'command']
-        const filemode_Read = 0x02
-        const filemode_Write = 0x01
-        const seekmode_Start = 0
-        const seekmode_End = 2
     `,
 
     emglken_file_delete(path_ptr, path_len) {
         const path = UTF8ToString(path_ptr, path_len)
-        const fref = {filename: path}
-        Dialog.file_remove_ref(fref)
+        Dialog.delete(path)
     },
 
+    emglken_file_exists__async: true,
     emglken_file_exists(path_ptr, path_len) {
-        const path = UTF8ToString(path_ptr, path_len)
-        const fref = {filename: path}
-        if (fref.filename === storyfile_name) {
-            return true
-        }
-        else {
-            return Dialog.file_ref_exists(fref)
-        }
+        return Asyncify.handleAsync(async () => {
+            const path = UTF8ToString(path_ptr, path_len)
+            if (path === storyfile_name) {
+                return true
+            }
+            return Dialog.exists(path)
+        })
     },
 
+    emglken_file_read__async: true,
     emglken_file_read(path_ptr, path_len, buffer) {
-        const path = UTF8ToString(path_ptr, path_len)
-        const fref = {filename: path}
-        let data
-        if (fref.filename === storyfile_name) {
-            data = storyfile_data
-        }
-        else if (Dialog.streaming) {
-            const fstream = Dialog.file_fopen(filemode_Read, fref)
-            if (fstream) {
-                fstream.fseek(0, seekmode_End)
-                const len = fstream.ftell()
-                data = new Uint8Array(len)
-                fstream.fseek(0, seekmode_Start)
-                fstream.fread(data)
-                fstream.fclose()
+        return Asyncify.handleAsync(async () => {
+            const path = UTF8ToString(path_ptr, path_len)
+            let data
+            if (path === storyfile_name) {
+                data = storyfile_data
             }
-        }
-        else {
-            data = Dialog.file_read(fref)
-        }
-        if (data) {
-            writeBuffer(buffer, data)
-            return true
-        }
-        return false
+            else {
+                data = await Dialog.read(path)
+            }
+            if (data) {
+                writeBuffer(buffer, data)
+                return true
+            }
+            return false
+        })
     },
 
     emglken_file_write_buffer(path_ptr, path_len, buf_ptr, buf_len) {
         const path = UTF8ToString(path_ptr, path_len)
-        const fref = {filename: path}
-        const buffer = HEAP8.subarray(buf_ptr, buf_ptr + buf_len)
-        if (Dialog.streaming) {
-            const fstream = Dialog.file_fopen(filemode_Write, fref)
-            if (fstream) {
-                const node_buffer = fstream.BufferClass.from(buffer)
-                fstream.fwrite(node_buffer)
-                fstream.fclose()
-            }
-        }
-        else {
-            data = Dialog.file_write(fref, buffer)
-        }
+        const data = HEAP8.subarray(buf_ptr, buf_ptr + buf_len)
+        Dialog.write(path, data)
     },
 
     emglken_get_glkote_event__async: true,
     emglken_get_glkote_event(buffer) {
         return Asyncify.handleAsync(async () => {
-            await new Promise(resolve => { glkote_event_ready = resolve })
+            if (!glkote_event_data) {
+                await new Promise(resolve => { glkote_event_ready = resolve })
+            }
             writeBufferJSON(buffer, glkote_event_data)
+            glkote_event_data = null
         })
     },
 
